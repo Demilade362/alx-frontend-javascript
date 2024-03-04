3ab81dbaa049c73befd195467b1b676af230705f
Demilade362 <ademolademilade362@gmail.com>
2024-03-04 15:55:19 +0100

    for loops linted

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (var value of array) {
-    var value = appendString + value;
+  for (let value of array) {
+    value = appendString + value;
   }
 
   return array;
