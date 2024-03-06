7f3ca3d0631c5d4c9578f38e14cc8ac0101c257b
Demilade362 <ademolademilade362@gmail.com>
2024-03-06 12:34:39 +0100

    3-all-fix-again

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const uploadPhotoPromise = uploadPhoto();
-  const createUserPromise = createUser();
-
-  Promise.all([uploadPhotoPromise, createUserPromise])
-    .then(([photoResponse, userResponse]) => {
-      console.log(`${photoResponse.body.firstName} ${userResponse.body.lastName}`);
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
     })
-    .catch(() => {
-      console.error('Signup system offline');
-    });
+
+    .catch(() => console.log('Signup system offline'));
 }
