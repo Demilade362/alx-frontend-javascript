bb78f02de4461f1d571d7ec1601fcad5eefa602d
Demilade362 <ademolademilade362@gmail.com>
2024-03-13 10:12:39 +0100

    made changes

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,19 @@ interface Teacher {
     [key: string]: any;
 }
 
+interface Directors extends Teacher{
+    numberOfReports: number
+}
+
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+  };
+  console.log(director1);
+
 const teacher3: Teacher = {
     firstName: 'John',
     fullTimeEmployee: false,
@@ -15,4 +28,4 @@ const teacher3: Teacher = {
     contract: false
 };
   
-  console.log(teacher3);
+console.log(teacher3);
